a104a6663d58c09698dd263f9101f1a64c113bfa
dev <devendra1andonly@gmail.com>
2022-12-13 20:03:06 +0530

    initial commit

---
+++ src/main/java/org/reunite/findmissingpeople/FindMissingPeopleApplication.java
@@ -0,0 +1,13 @@
+package org.reunite.findmissingpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMissingPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMissingPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/reunite/findmissingpeople/FindMissingPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reunite.findmissingpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMissingPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
